4a589819f75ef9abb45884c4932645c25e84155b
Chelsea Roston <cmroston@gmail.com>
2021-06-14 12:04:28 -0400

    adds stories index

---
+++ src/views/StoryIndex.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="story">
+    <div v-for="story in stories" :key="story.id">{{story.name}}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      stories: [],
+      
+    };
+  },
+  methods: {
+    indexStories: function () {
+      axios.get("/api/stories").then((response) => {console.log(response.data); 
+      this.stories = response.data;
+      });
+    }
+  }
+}
+<script>
